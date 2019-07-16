425f16487f2e2805a0622b2a0d758d4b080d058e
Gabriel Miguel <gmiguel14@gmail.com>
2019-07-16 11:38:39 -0300

    Adicionar melhorias ao JS

--- scripts.js
+++ scripts.js
@@ -3,34 +3,27 @@ let randomColor;
 let element;
 
 for (let i = 0; i < 6; i++) {
-    color.push(['rgb(' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ')']);
-    element = document.getElementById("color" + i);
+    color.push(['rgb(' + (Math.floor(Math.random() * 256)) + ', '
+     + (Math.floor(Math.random() * 256)) + ', '
+     + (Math.floor(Math.random() * 256)) + ')']);
+
+    element = document.querySelector("#color" + i);
     element.style.backgroundColor = color[i];
     randomColor = color[Math.floor(Math.random() * color.length)]
-    document.getElementById('color').innerHTML = randomColor;
+    document.querySelector('#color').innerHTML = randomColor;
 }
 
 function boxClick(id, backgroundColor) {
 
     let randomColorString = JSON.stringify(randomColor).replace(/[\[\]']+/g, '').replace(/['"]+/g, '');
-    console.log(backgroundColor);
-    console.log(randomColorString);
 
     if (randomColorString == backgroundColor) {
-        document.getElementById("color0").style.backgroundColor = randomColor;
-        document.getElementById("color0").style.display = 'block';
-        document.getElementById("color1").style.backgroundColor = randomColor;
-        document.getElementById("color1").style.display = 'block';
-        document.getElementById("color2").style.backgroundColor = randomColor;
-        document.getElementById("color2").style.display = 'block';
-        document.getElementById("color3").style.backgroundColor = randomColor;
-        document.getElementById("color3").style.display = 'block';
-        document.getElementById("color4").style.backgroundColor = randomColor;
-        document.getElementById("color4").style.display = 'block';
-        document.getElementById("color5").style.backgroundColor = randomColor;
-        document.getElementById("color5").style.display = 'block';
+        for(i = 0; i <= 5; i++){
+            document.querySelector('#color' + i).style.backgroundColor = randomColor;
+            document.querySelector('#color' + i).style.display = 'block';
+        }
     } else {
-        document.getElementById(id).style.display = 'none';
+        document.querySelector('#' + id).style.display = 'none';
     }
 };
 
